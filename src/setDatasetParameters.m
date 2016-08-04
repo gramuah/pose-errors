2a8c9912b9ae40c93a7b8617849ade17a858f8fe
carolinauah <carolina.redondo.cabrera@gmail.com>
2016-08-04 15:33:00 +0200

    1 commint

--- src/setDatasetParameters.m
+++ src/setDatasetParameters.m
@@ -15,10 +15,10 @@ switch dataset
     sourceDir = '../PASCAL3D+';
     dataset_params.imset = 'test';  % set used for analysis
     dataset_params.imdir = [sourceDir '/data/PASCAL3D+_release1.1/PASCAL/VOCdevkit/VOC2012/JPEGImages']; % needs to be set for your computer
-    dataset_params.VOCsourcepath = './eval_code/src/VOCcode';  % change this for later VOC versions
+    dataset_params.VOCsourcepath = './src/VOCcode';  % change this for later VOC versions
     dataset_params.VOCset = 'PASCAL3D';
     dataset_params.dataBaseDir = [sourceDir '/data/PASCAL3D+_release1.1'];
-    addpath(dataset_params.VOCsourcepath); %, 'boat'
+    addpath(dataset_params.VOCsourcepath);
     dataset_params.annotationdir = '../annotations';
     dataset_params.confidence_threshold = -Inf; % minimum confidence to be included in analysis (e.g., set to 0.01 to improve speed)
 
